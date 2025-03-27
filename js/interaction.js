671c04f995ca028baf4a9558cdde16baabd15e03
박정인 <jeongin@hivelab.co.kr>
2025-03-27 16:11:07 +0900

    Fix: 배너 fetch로 호출 완료 후 gsap 호출로 변경 => MutationObserver option 사용

--- js/interaction.js
+++ js/interaction.js
@@ -1,21 +1,27 @@
-const bannerTitle = document.querySelector(".banner-title");
-console.log(document.querySelector(".banner-title"));
-
-gsap.from(".banner-title", { 
-  rotationY: 36, 
-  opacity: 0, 
-  duration: 0.8, 
-  yPercent: 100, 
-  stagger: 0.1, 
-  ease: "Expo.easeOut",
-  onComplete: () => {
-    gsap.to(".banner-desc", {
-      opacity: 1, 
-      y: 0, 
-      duration: 1
+const observer = new MutationObserver((mutations, obs) => {
+  const bannerTitle = document.querySelector(".banner-title");
+  if (bannerTitle) {
+    gsap.from(".banner-title", { 
+      rotationY: 36, 
+      opacity: 0, 
+      duration: 0.8, 
+      yPercent: 100, 
+      stagger: 0.1, 
+      ease: "Expo.easeOut", 
+      onComplete: () => {
+        gsap.to(".banner-desc", { 
+          opacity: 1, 
+          y: 0, 
+          duration: 1
+        });
+      }
     });
+    obs.disconnect();
   }
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
+
 
 const swiper = new Swiper(".swiper", {
   loop: true,
